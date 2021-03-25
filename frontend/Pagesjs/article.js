9140a14db831e5617d78be3375a66a66390b3297
sousouben <s.mouatakide@laposte.net>
2021-03-25 09:56:14 +0100

    création de page article

--- frontend/Pagesjs/article.js
+++ frontend/Pagesjs/article.js
@@ -3,31 +3,48 @@ function getId(){ console.log('ok');
     const params = new URLSearchParams(querystring);
     const iD = params.get("id");
     console.log(iD);
+    Produit(iD);
 }
 
-getId();
+let URLAPI = "http://localhost:3000/api/teddies/";//Au choix entre : "cameras" - "furniture" - "teddies"
+
+function Produit(iD) {
+    fetch(URLAPI+iD).then(responce=>responce.json())// fetch va chercher l'URL + transformation de la réponse en json
+    .then(responce=>insertProduit(responce))//.then sert à attacher les functions 
+    }
+
+function insertProduit(responce){
+    let articleProduit= document.getElementById("article_ours");  
+    console.log(responce)  
+        articleProduit.innerHTML += '<article id="page_produit">' 
+        + '<section class="produit_page1">' 
+        +'<div class="img_produit">'
+        + '<img src="'+responce.imageUrl +'" >' 
+        + '</div>'
+        + '<div class="info_produit">'
+        +'<div class="nom_produit">'
+        + '<h3>'+responce.name+'</h3>'
+        +'</div>' 
+        + '<div class="prix-produit">'
+        + '<p>'+ responce.price/100+'€</p>'
+         + '</div>'
+         +'<div class="description">'
+         +'<h4>'+responce.description+'</h4>'
+         +'<div>'
+         +'<select id="select"></select>'
+         + '<a href="#">'+ 'ajouter au panier'+ '</a>'
+         +'</div>' 
+         +'</section>' 
+         +'</article>';
+         let select=document.getElementById("select");
+            for(let i=0 ; i<responce.colors.length; i++){
+                select.innerHTML+='<option value="'+responce.colors[i]+ '">'+responce.colors[i]+'</option>'
+
+            }
+         
+    
+}
+
+window.onload= getId();
 
 
-
-/*<article id="page_produit">
-                <section class="produit_page1">
-                    <div class="img_produit">
-                        <img src="" alt="">
-                    </div>
-                    <div class="info_produit">
-                        <p></p>
-                        <div class="nom_produit">
-                            <h3></h3>
-                        </div>
-                        <div class="prix-produit">
-                            <p></p>
-                        </div>
-                        <div class="description">
-                          <h4></h4>
-                          <p> </p>
-                           <h4></h4>
-                            
-                        </div>
-                    </div>
-                </section>
-            </article>*/
--- frontend/produit.js
+++ frontend/produit.js
@@ -10,7 +10,7 @@ function Produit() {
 function insertProduit(responce){
     let articleProduit= document.getElementById("listeProduit");
     for(let i=0 ; i<responce.length; i++){
-        articleProduit.innerHTML += '<article id="produit">' + '<div class="main_produit">' +'<div class="image">'+ '<section class="produit_item">' +'<div class="img_produit">' + '<img src="'+responce[i].imageUrl +'" >' + '</div>'+ '<div class="nom_produit">'+ '<h3>'+responce[i].name+'</h3>'+'</div>' + '<div class="prix-produit">'+ '<p>'+ responce[i].price/100 +'€</p>'+ '</div>'+'<div>'+ '<a href="pages/Page%20Produit%20.html?id='+ responce[i]._id +'">'+ 'voir la fiche produit'+ '</a>'+'</div>' +'</section>' +'</div>' + '</div>' + '</article>';
+        articleProduit.innerHTML += '<article id="produit">' + '<div class="main_produit">' +'<div class="image">'+ '<section class="produit_item">' +'<div class="img_produit">' + '<img src="'+responce[i].imageUrl +'" >' + '</div>'+ '<div class="nom_produit">'+ '<h3>'+responce[i].name+'</h3>'+'</div>' + '<div class="prix-produit">'+ '<p>'+ responce[i].price/100 +'€</p>'+ '</div>'+'<div>'+ '<a href="pages/Page%20article.html?id='+ responce[i]._id +'">'+ 'voir la fiche produit'+ '</a>'+'</div>' +'</section>' +'</div>' + '</div>' + '</article>';
     }
 }
 
