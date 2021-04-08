b92e6e5b388b006dd4144830f56176a6af64b10d
sousouben <s.mouatakide@laposte.net>
2021-04-08 09:50:54 +0200

    création formulaire html

--- frontend/Pagesjs/panier.js
+++ frontend/Pagesjs/panier.js
@@ -1,10 +1,15 @@
 //    Affichage des articles mis au panier dans la page panier
-function getId(){ console.log('articles mis au panier');
+function getProduitsPaniers(){ console.log('articles mis au panier');
     let affichagePanier= localStorage.getItem("Panier");
-    affichagePanier=JSON.parse(affichagePanier);
+    if(affichagePanier == null){
+        document.getElementById('info').style.display='none';
+    }else{
+        affichagePanier=JSON.parse(affichagePanier);
     for(let i=0 ; i<affichagePanier.length; i++){
         Produit(affichagePanier[i]);
     }
+    }
+    
 }
 
 let URLAPI = "http://localhost:3000/api/teddies/";
@@ -34,7 +39,7 @@ function insertPanier(responce){
         +'</div>'
                
 }
-window.onload= getId();
+window.onload= getProduitsPaniers();
 
 
 
@@ -60,95 +65,10 @@ window.onload= getId();
             +'</div>' 
             +'</div>'      
 }
-window.onload=getId();
+window.onload=getId();*/
         
 
 
             
         
-               <div class="payer">
-                    <div class="container container__info">
-                        <div class="container container__payer">
-                            
-                            <div class="info">
-                                <h2>Informations de livraison</h2>
-                                <div class="form__row">
-                                    <div class="form__col">
-                                        <label for="firstname">Prénom :</label>
-                                        <input type="text" id="firstname" name="user_firstname">
-                                    </div>
-                                    <div class="form__col">
-                                        <label for="name">Nom :</label>
-                                        <input type="text" id="name" name="user_name">
-                                    </div>
-                                </div>
-                                <div class="form__row">
-                                    <div class="form__col">
-                                        <label for="adress">Adresse :</label>
-                                        <input type="text" id="adress" name="user_adress">
-                                    </div>
-                                    <div class="form__col">
-                                        <label for="city">Ville :</label>
-                                        <input type="text" id="city" name="user_city">
-                                    </div>
-                                </div>
-                                <div class="form__row">
-                                    <div class="form__col">
-                                        <label for="code_postal">Code Postal :</label>
-                                        <input type="number" id="code_postal" name="user_code_postal">
-                                    </div>
-                                    <div class="form__col">
-                                        <label for="mail">e-mail :</label>
-                                        <input type="email" id="mail" name="user_mail">
-                                    </div>
-                                </div>
-                            </div>
-                           
-                            <div class="mode_payment">
-                                <h2>Informations de paiement</h2>
-                                <div class="form__row">
-                                    <div class="form__col">
-                                        <label for="card_cvv">Méthode de Paiement :</label>
-                                        <div class="card__type__container">
-                                            <div>
-                                                <img id="cardType" class="card__type" src="../logo/visa_card.jpg" alt="">
-                                            </div>
-                                            <select name="select__card" id="card__option" class="card__select">
-                                                <option value="0">VISA</option>
-                                                <option value="1">CARTE BLEUE</option>
-                                                <option value="2">MASTERCARD</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form__row">
-                                    <div class="form__col">
-                                        <label for="card_number">N° de carte :</label>
-                                        <input type="number" id="card_number" name="card_number">
-                                    </div>
-                                    <div class="form__col">
-                                        <label for="card_cvv">CVV :</label>
-                                        <input type="number" id="card_cvv" name="card_cvv">
-                                    </div>
-                                </div>
-                                <div class="form__row">
-                                    <div class="form__col">
-                                        <label for="card_date">Date d'expiration :</label>
-                                        <div class="exp__container">
-                                            <input type="number" id="card_date" name="card_date">
-                                            <input type="number" id="card_year" name="card_year">
-                                        </div>
-                                    </div>
-                                    <div class="form__col">
-                                        <label for="card_name">Nom :</label>
-                                        <input type="text" id="card_name" name="card_name">
-                                    </div>
-                                </div>
-                            </div>                            
-                        </div>
-                        <button class="button">Confirmer le paiement</button>
-                                      
-                    </div>
-                </div>
-            </div>
-            */
+              
