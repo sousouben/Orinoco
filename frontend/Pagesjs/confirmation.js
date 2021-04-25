e76637516f948a4ab6085c5951345cca23c3d489
sousou ben <s.mouatakide@laposte.net>
2021-04-25 10:17:33 +0200

    css page confirmation + fin du plan test

--- frontend/Pagesjs/confirmation.js
+++ frontend/Pagesjs/confirmation.js
@@ -1,14 +1,13 @@
 // récupération du numéro de commande + message de commande
 function ordreDeConfirmation(){
 const orderId = localStorage.getItem('numero de commande');
-console.log(orderId);
+
 const messageDeConfirmation = document.getElementById('orderId');
 messageDeConfirmation.innerHTML = 'Votre commande est confirmée <br> sous le n°: '+ orderId;
-console.log(messageDeConfirmation);
+
 const totalPrix = localStorage.getItem("totalOrder");
 const PrixConfirm = document.getElementById('total-prix');
 PrixConfirm.innerHTML = 'Prix total :' + totalPrix / 100 + '€' + '<br>'+ '<br>'+ '<p>Merci d\'avoir choisi Orinounours pour vos achats</p>'+ '<p> A bientôt chez Orinoco !</p>';
-
 }
 
 ordreDeConfirmation()
