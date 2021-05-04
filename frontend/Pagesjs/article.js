ee80a777f453ca4f16f8eae7bf46019663d03000
sousou ben <s.mouatakide@laposte.net>
2021-05-04 16:35:51 +0200

    modification page produit et article

--- frontend/Pagesjs/article.js
+++ frontend/Pagesjs/article.js
@@ -1,77 +1,84 @@
-let idProduct;
-
-function getId(){ //création d'une fonction pour avoir un identifiant pour chaque produit
-    const querystring = window.location.search;//Renvoie un objet Location contenant des informations concernant l'URL actuelle du document et fournit des méthodes pour modifier cette URL.search La partie de l'URL qui suit le symbole « ? », avec ce symbole inclus
-    const params = new URLSearchParams(querystring);// fournit des informations pour l'analyse et le formatage des chaînes de requête d'URL.
-    const iD = params.get("id"); // La syntaxe get permet de lier une propriété d'un objet à une fonction qui sera appelée lorsqu'on accédera à la propriété.  
-    idProduct=iD;//let idProduct devient iD
-    Produit(iD);
-    }
-
 let URLAPI = "http://localhost:3000/api/teddies/";
 
+let idProduct;
+
+function getId() { 
+    const querystring = window.location.search;
+    const params = new URLSearchParams(querystring);
+    const iD = params.get("id"); 
+    idProduct = iD;
+    Produit(iD);
+}
+
 function Produit(iD) {//création d'une fonction pour récupérer les identifiants pour chaque produit
-    fetch(URLAPI+iD)
-    .then(response=>response.json())
-    .then(data=>insertProduit(data))
-    .catch(error => {        
-        console.log("Il y a une erreur :"+ error.stack);
-    })    
+    fetch(URLAPI + iD)
+        .then(response => response.json())
+        .then(response => {
+            insertProduit(response);
+            idProduct = response;
+        })
+        .catch(error => {
+            console.log("Il y a une erreur :" + error.stack);
+        })
 };
 
-function insertProduit(data){    
-    let articleProduit= document.getElementById("article_ours");  
-    console.log(data);  //pour avoir un objet avec toutes les information sur le produit
-        articleProduit.innerHTML += '<article id="page_produit">' 
-        + '<section class="produit_page1">' 
-        +'<div class="img_produit">'
-        + '<img src="'+ data.imageUrl +'" >' 
+function insertProduit(response) {
+    let articleProduit = document.getElementById("article_ours");
+    articleProduit.innerHTML += '<article id="page_produit">'
+        + '<section class="produit_page1">'
+        + '<div class="img_produit">'
+        + `<img src="${response.imageUrl}" >`
         + '</div>'
         + '<div class="info_produit">'
-        +'<div class="nom_produit">'
-        + '<h3>'+ data.name+'</h3>'
-        +'</div>' 
-        +'<hr></hr>'
+        + '<div class="nom_produit">'
+        + `<h3>${response.name} </h3>`
+        + '</div>'
+        + '<hr></hr>'
         + '<div class="prix-produit">'
-        + '<p>'+ data.price/100+'€</p>'
-         + '</div>'
-         +'<div class="description">'
-         +'<h4>Description</h4>'
-         +'<p>'+ data.description+'</p>'
-         +'<div class="bouton">'
-         +'<select id="select"><option selected disabled value="">choose..</option></select>'
-         + '<a href="#">'
-         + '<button id="ajouter"> ajouter au panier </button>'
-         + '</a>'
-         +'</div>' 
-         +'</section>' 
-         +'</article>';
-        
+        + `<p>${response.price / 100} €</p>`
+        + '</div>'
+        + '<div class="description">'
+        + '<h4>Description</h4>'
+        + `<p>${response.description}</p>`
+        + '<div class="bouton">'
+        + '<select id="select"><option selected disabled value="">choose..</option></select>'
+        + '<a href="#">'
+        + '<button id="ajouter"> ajouter au panier </button>'
+        + '</a>'
+        + '</div>'
+        + '</section>'
+        + '</article>';
 
-         let listeColor = document.getElementById("select");//création d'option de séléction des couleurs
-            for(let i = 0;i < data.colors.length; i++){
-                listeColor.innerHTML+='<option value="'+ data.colors[i]+ '">'                
-                + data.colors[i]
-                +'</option>';                
-            }
-            
-            document.getElementById("ajouter").addEventListener("click",function(){
-                let panier = JSON.parse(localStorage.getItem("Panier"))//La méthode JSON.parse() analyse une chaîne de caractères JSON et construit la valeur JavaScript ou l'objet décrit par cette chaîne.
-                if(panier == null){ 
-                    panier = [];
-                    }
-                panier.push(idProduct); //ajout du produit dans le panier               
-                localStorage.setItem("Panier",JSON.stringify(panier));//La méthode JSON.stringify() convertit une valeur JavaScript en chaîne JSON.
-                if(window.confirm("Article bien ajouté au panier. Souhaitez vous consulter votre panier ?")){
-                    window.location.href = "Panier.html";
-                } else {
-                    window.location.href = "../index.html";
-                }                
-            });
+
+    let listeColor = document.getElementById("select");//création d'option de séléction des couleurs
+    for (let i = 0; i < response.colors.length; i++) {
+        listeColor.innerHTML += '<option value="' + response.colors[i] + '">'
+            + response.colors[i]
+            + '</option>';
+    }
+
+    document.getElementById("ajouter").addEventListener("click", function () {
+        let panier = JSON.parse(localStorage.getItem("Panier"))
+        if (panier == null) {
+            panier = [];
+        }
+        panier.push({
+            id: idProduct._id,
+            imageUrl: idProduct.imageUrl,
+            name: idProduct.name,
+            price: idProduct.price
+        }); 
+        localStorage.setItem("Panier", JSON.stringify(panier));
+        if (window.confirm("Article bien ajouté au panier. Souhaitez vous consulter votre panier ?")) {
+            window.location.href = "Panier.html";
+        } else {
+            window.location.href = "../index.html";
+        }
+    });
 }
 
 // Appel de fonctions
-window.onload= getId();
+window.onload = getId();
 
 
-   
+
--- frontend/produit.js
+++ frontend/produit.js
@@ -2,7 +2,47 @@
 **********************************************/
 let URLAPI = "http://localhost:3000/api/teddies";//Au choix entre : "cameras" - "furniture" - "teddies"
 
-function Produit() {             
+window.onload = function () {
+    fetch(URLAPI)//fetch  récupérer des ressources à travers le réseau de manière asynchrone.
+        .then(response => response.json())// fetch va chercher l'URL + transformation de la réponse en json
+        .then(response => {
+            let element = document.getElementById("listeProduit");//Lien avec la page index HTML
+
+            for (let i = 0; i < response.length; i++) {
+                element.innerHTML +='<article id="produit">'//création de la structure index HTML
+                + '<a href="pages/article.html?id=' + response[i]._id + '">'
+                + '<div class="main_produit">'
+                + '<div class="image">'
+                + '<section class="produit_item">'
+                + '<div class="img_produit">'
+                + '<img src="' + response[i].imageUrl + '" >'
+                + '</div>'
+                + '<div class="nom_produit">'
+                + '<h3>' + response[i].name + '</h3>'
+                + '</div>'
+                + '<div class="prix-produit">'
+                + '<p>' + response[i].price / 100 + '€</p>'
+                + '</div>'
+                + '<div>'            
+                + '<button> voir ce produit </button>'            
+                + '</div>'
+                + '</section>'
+                + '</div>'
+                + '</div>'
+                + '</a>'
+                + '</article>';
+                    
+            }
+        })
+        .catch(error => {
+            //Récupération des messages d'erreurs en cas de problèmes(s) avec la ligne grace à .stack
+            console.log("Il y a une erreur :"+ error.stack);            
+        });
+}
+
+
+
+/*function Produit() {             
     fetch(URLAPI) //fetch  récupérer des ressources à travers le réseau de manière asynchrone.
         .then(response => response.json())// fetch va chercher l'URL + transformation de la réponse en jso
         .then(data => insertProduit(data))//.then sert à attacher les functions  
@@ -41,4 +81,4 @@ function insertProduit(data) {
 }
 
 // Appel de fonctions
-window.onload = Produit();
+window.onload = Produit();*/
