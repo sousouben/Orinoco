51590dd1db12cc201c12b1f02ff4f2652b02a487
sousou ben <s.mouatakide@laposte.net>
2021-05-05 16:54:59 +0200

    minimisation de la page panier

--- frontend/produit.js
+++ frontend/produit.js
@@ -1,12 +1,12 @@
 /*Génération de l'URL de l'API selon le choix de produit à vendre
 **********************************************/
-let URLAPI = "http://localhost:3000/api/teddies";//Au choix entre : "cameras" - "furniture" - "teddies"
+let URLAPI = "http://localhost:3000/api/teddies";
 
 window.onload = function () {
-    fetch(URLAPI)//fetch  récupérer des ressources à travers le réseau de manière asynchrone.
-        .then(response => response.json())// fetch va chercher l'URL + transformation de la réponse en json
+    fetch(URLAPI)
+        .then(response => response.json())
         .then(response => {
-            let element = document.getElementById("listeProduit");//Lien avec la page index HTML
+            let element = document.getElementById("listeProduit");
 
             for (let i = 0; i < response.length; i++) {
                 element.innerHTML +='<article id="produit">'//création de la structure index HTML
@@ -40,45 +40,3 @@ window.onload = function () {
         });
 }
 
-
-
-/*function Produit() {             
-    fetch(URLAPI) //fetch  récupérer des ressources à travers le réseau de manière asynchrone.
-        .then(response => response.json())// fetch va chercher l'URL + transformation de la réponse en jso
-        .then(data => insertProduit(data))//.then sert à attacher les functions  
-        .catch(error => {
-            //Récupération des messages d'erreurs en cas de problèmes(s) avec la ligne grace à .stack
-            console.log("Il y a une erreur :"+ error.stack);            
-        })
-};
-
-function insertProduit(data) {     
-    let articleProduit = document.getElementById("listeProduit");//Lien avec la page index HTML
-    for (let i = 0; i < data.length; i++) {
-        articleProduit.innerHTML += '<article id="produit">'//création de la structure index HTML
-            + '<a href="pages/article.html?id=' + data[i]._id + '">'
-            + '<div class="main_produit">'
-            + '<div class="image">'
-            + '<section class="produit_item">'
-            + '<div class="img_produit">'
-            + '<img src="' + data[i].imageUrl + '" >'
-            + '</div>'
-            + '<div class="nom_produit">'
-            + '<h3>' + data[i].name + '</h3>'
-            + '</div>'
-            + '<div class="prix-produit">'
-            + '<p>' + data[i].price / 100 + '€</p>'
-            + '</div>'
-            + '<div>'            
-            + '<button> voir ce produit </button>'            
-            + '</div>'
-            + '</section>'
-            + '</div>'
-            + '</div>'
-            + '</a>'
-            + '</article>';
-    }
-}
-
-// Appel de fonctions
-window.onload = Produit();*/
